cf995e3d7a4f6e824e87d8d2866255498ba3fdc3
EgorChernov3140 <egorchernov3140@gmail.com>
2023-03-10 15:17:48 +0300

    Add index.js and routes, also create user model

---
+++ homeworks/K33401/Chernov_Egor/hw2/index.js
@@ -0,0 +1,11 @@
+let express = require('express');
+
+let indexRouter = require('./routes/index');
+let usersRouter = require('./routes/user');
+
+let app = express();
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
