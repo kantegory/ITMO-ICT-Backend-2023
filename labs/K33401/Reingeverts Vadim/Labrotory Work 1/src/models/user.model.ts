352bcb7179b5ca596c7e514d5ab9f96fe093d992
LiprikON2 <44316968+LiprikON2@users.noreply.github.com>
2023-04-10 17:52:30 +0300

    init: delete everything

--- labs/K33401/Reingeverts Vadim/Labrotory Work 1/src/models/user.model.ts
+++
@@ -1,3 +0,0 @@
-import { DataTypes } from "sequelize";
-
-console.log(DataTypes);
--- labs/K33401/Reingeverts Vadim/Labrotory Work 1/src/vite-env.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="vite/client" />
-declare const __dirname: string;
-declare const test: string;
