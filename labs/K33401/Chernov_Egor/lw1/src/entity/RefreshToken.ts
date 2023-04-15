a9df154294d80651215279574cadca1be63dd23b
EgorChernov3140 <egorchernov3140@gmail.com>
2023-04-16 00:28:06 +0300

    LW1: add RefreshToken table to entity

---
+++ labs/K33401/Chernov_Egor/lw1/src/entity/RefreshToken.ts
@@ -0,0 +1,14 @@
+import {Entity, PrimaryColumn, OneToOne} from "typeorm"
+import {User} from "./User";
+
+@Entity("refresh_token")
+export class RefreshToken {
+    @PrimaryColumn()
+    token: string
+
+    @OneToOne(() => User, {
+        cascade: true,
+        onDelete: "SET NULL"
+    })
+    user: User
+}
