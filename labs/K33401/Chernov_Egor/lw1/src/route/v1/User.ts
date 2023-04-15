6b7df890b62367f2509adb27b9d311692730c16b
EgorChernov3140 <egorchernov3140@gmail.com>
2023-04-15 23:21:18 +0300

    LW1: realize routes

--- labs/K33401/Chernov_Egor/lw1/src/route/v1/User.ts
+++ labs/K33401/Chernov_Egor/lw1/src/route/v1/User.ts
@@ -1,32 +1,34 @@
 import express from "express"
-import TestController from "../../controller/test/index"
-// import passport from "../../../middlewares/passport"
+// import TestController from "../../controller/test/index"
+import UserController from "../../controller/v1/UserController"
+import portfolioRouter from "./Portfolio";
+// import passport from "../../../middleware/passport"
 
-const router: express.Router = express.Router()
-
-// start test
-const testController = new TestController()
-
-router.route('/test')
-    .get(testController.get)
-// end test
-
-
-// const controller: UserController = new UserController()
+// Test
+// const router: express.Router = express.Router()
+// const testController = new TestController()
 //
-// router.route('/')
-//     .post(controller.post)
-//
-// router.route('/profile')
-//     .get(passport.authenticate('jwt', { session: false }), controller.me)
-//
-// router.route('/profile/:id')
-//     .get(controller.get)
-//
-// router.route('/login')
-//     .post(controller.auth)
-//
-// router.route('/refresh')
-//     .post(controller.refreshToken)
+// router.route('/test')
+//     .get(testController.get)
 
-export default router
+// Create router and controller
+const userRoutes: express.Router = express.Router()
+const userController: UserController = new UserController()
+
+// User routes
+userRoutes.route('/list')
+    .get(userController.get_all)
+
+userRoutes.route('/specific')
+    .get(userController.get)
+
+userRoutes.route('/registration')
+    .get(userController.post_create_user)
+
+userRoutes.route('/login')
+    .get(userController.post_login_user)
+
+portfolioRouter.route('/delete')
+    .get(userController.delete)
+
+export default userRoutes
--- labs/K33401/Chernov_Egor/lw1/src/route/v1/index.ts
+++ labs/K33401/Chernov_Egor/lw1/src/route/v1/index.ts
@@ -1,8 +1,12 @@
 import express from "express"
 import userRoutes from "./User"
+import portfolioRoutes from "./Portfolio"
+import coinRoutes from "./Coin"
 
 const router: express.Router = express.Router()
 
 router.use('/users', userRoutes)
+router.use('/portfolio', portfolioRoutes)
+router.use('/coins', coinRoutes)
 
 export default router
