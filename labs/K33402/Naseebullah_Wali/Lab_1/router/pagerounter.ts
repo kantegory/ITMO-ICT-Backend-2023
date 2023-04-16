77352974c86c3394e71fedd18247e65b4225f120
Naseebullah-Wali <70603493+Naseebullah-Wali@users.noreply.github.com>
2023-04-16 04:53:15 +0300

    Lab_1 Ready

---
+++ labs/K33402/Naseebullah_Wali/Lab_1/router/pagerounter.ts
@@ -0,0 +1,34 @@
+import express from "express"
+// import {show,UserPost,ShowUsers,FindById,UpdateUser,DeleteUser} from "../Controllers/userControllers";
+import { userControllers } from "../controllers/pageControllers";
+import {body, validationResult} from 'express-validator'
+import jwtout from "../middleware/jwtout";
+const rout:express.Router = express.Router();
+// rout.get('/',userControllers.show)
+
+rout.post('/users', userControllers.UserPost);
+
+
+//for getting all users in table
+rout.get('/users',  userControllers.ShowUsers)
+
+
+//for getting specific user in table
+rout.get('/users/:id',userControllers.FindById)
+
+//for updating data
+rout.put('/users/:id',userControllers.UpdateUser)
+
+//for deleting user
+rout.delete('/users/:id',userControllers.DeleteUser)
+
+rout.post('/login',[
+    body('username').not().isEmpty().withMessage('username is Required'),
+    body('email').not().isEmpty().withMessage('email is Required')
+],userControllers.Login)
+
+
+rout.get('/jwt',jwtout, (req,res)=>{
+    console.log('test')
+})
+export default rout;
