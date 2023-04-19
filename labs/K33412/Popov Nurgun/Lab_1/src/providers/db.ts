1343340f52e48ea437c483863a2deded982cfa65
Nurgunpopov <71345993+Nurgunpopov@users.noreply.github.com>
2023-04-20 00:08:33 +0300

    Update db.ts

--- labs/K33412/Popov Nurgun/Lab_1/src/providers/db.ts
+++ labs/K33412/Popov Nurgun/Lab_1/src/providers/db.ts
@@ -26,7 +26,7 @@ async function testConnection() {
         await sequelize.authenticate();
         console.log('Connection has been established successfully.');
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        throw new Error('Unable to connect to the database');
     }
 }
 
