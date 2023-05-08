e0146c99ad4506361186b98aa28ced110e5431c7
chiclassie <az20022002@icloud.com>
2023-05-08 22:05:21 +0300

    feat: add lab2 solution

---
+++ labs/K33402/AZaytseva/3/main/src/seeders/20230402144343-users.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Users', [
+      {
+        firstName: 'First',
+        lastName: 'User',
+        email: 'first@example.com',
+        password: 'Testtest123',
+        role: 1,
+        dob: new Date('2002-02-02'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        firstName: 'Second',
+        lastName: 'User',
+        email: 'second@example.com',
+        password: 'Testtest123',
+        role: 2,
+        dob: new Date('2002-02-02'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        firstName: 'Third',
+        lastName: 'User',
+        email: 'third@example.com',
+        password: 'Testtest123',
+        role: 3,
+        dob: new Date('2002-02-02'),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Users', null, {});
+  }
+};
