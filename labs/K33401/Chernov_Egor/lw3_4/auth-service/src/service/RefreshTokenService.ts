73b3b7aeb1b1a3cd2dac111da68856792824affe
EgorChernov3140 <egorchernov3140@gmail.com>
2023-05-24 18:49:19 +0300

    LW3: init services

---
+++ labs/K33401/Chernov_Egor/lw3_4/auth-service/src/service/RefreshTokenService.ts
@@ -0,0 +1,38 @@
+import {Repository} from "typeorm"
+import {RefreshToken} from "../entity/RefreshToken"
+import {AppDataSource} from "../data-source"
+
+
+class RefreshTokenService {
+    private refreshTokenRepository: Repository<RefreshToken>
+
+    constructor() {
+        this.refreshTokenRepository = AppDataSource.getRepository(RefreshToken)
+    }
+
+    async get(user) {
+        const refreshToken = await this.refreshTokenRepository.findOne({
+            where: {
+                user: user
+            }})
+        if (refreshToken) {
+            return refreshToken.token
+        }
+        throw new Error("Refresh token does not exist for current user")
+    }
+
+    async create(user: any, token: string) {
+        const refreshToken = new RefreshToken()
+        refreshToken.token = token
+        refreshToken.user = user
+        await this.refreshTokenRepository.save(refreshToken)
+        return token
+    }
+
+    async update(user: any, refreshToken: string) {
+        await this.refreshTokenRepository.update({user: user}, {token: refreshToken})
+        return refreshToken
+    }
+}
+
+export default RefreshTokenService
