c19b669871c8e65f2d7054ee1e23aef3034f31e3
nikiforovakyunney <nik.kyunney@mail.ru>
2023-05-28 21:21:48 +0300

    add lab 2

---
+++ labs/K33402/Nikiforova_Kyunney/lab_2/src/routes/events/Event.ts
@@ -0,0 +1,20 @@
+import express from "express"
+import { EventController } from "../../controllers/events/Event"
+
+const routes: express.Router = express.Router()
+
+const controller: EventController = new EventController()
+
+routes.route('/filter')
+    .get(controller.filter)
+
+routes.route('/')
+    .get(controller.calendar)
+
+routes.route('/author/:id')
+    .get(controller.author)
+
+routes.route('/:id')
+    .get(controller.get)
+
+export default routes
