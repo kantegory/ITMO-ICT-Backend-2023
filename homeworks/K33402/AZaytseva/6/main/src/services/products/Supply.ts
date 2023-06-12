0ba4df0b4bbc743b42d66b2b4036f630a1b4d9c0
chiclassie <az20022002@icloud.com>
2023-06-12 15:10:39 +0300

    feat(hw6): add lab4 solution

---
+++ homeworks/K33402/AZaytseva/6/main/src/services/products/Supply.ts
@@ -0,0 +1,68 @@
+import { Op } from "sequelize";
+import Supply from "../../models/products/Supply";
+
+interface SupplyCreateRequest {
+  quantity: number;
+  productId: number;
+  dateOfSupply: Date;
+}
+
+interface SuppliedRequestParams {
+    productId: number;
+    dateFrom?: Date;
+    dateTo?: Date;
+}
+
+class SupplyRepository {
+    async getAll() : Promise<Supply[]> {
+        const supplies = await Supply.findAll()
+
+        return supplies
+    }
+
+    async getSuppliedAmount(params: SuppliedRequestParams): Promise<number> {
+        const amount = await Supply.sum('quantity', {
+            where: {
+                productId: params.productId,
+                dateOfSupply: {
+                    [Op.gte]: params.dateFrom ?? new Date(0),
+                    [Op.lte]: params.dateTo ?? new Date(),
+                }
+            }
+        })
+
+        return amount
+    }
+
+    async getById(id: number) : Promise<Supply> {
+        const supply = await Supply.findByPk(id)
+
+        if (supply) {
+          return supply.toJSON()
+        }
+
+        throw new Error('Not found!')
+    }
+
+    async create(supplyData: Partial<SupplyCreateRequest>): Promise<Supply> {
+        const supply = await Supply.create(supplyData)
+        return supply.toJSON()
+    }
+
+    async deleteById(id: number): Promise<boolean> {
+        const deletedRows = await Supply.destroy({ where: { id } })
+        return deletedRows > 0;
+    }
+
+    async deleteByProductId(productId: number): Promise<boolean> {
+        const deletedRows = await Supply.destroy({ where: { productId } })
+        return deletedRows > 0;
+    }
+
+    async updateById(id: number, supplyData: Partial<SupplyCreateRequest>): Promise<boolean> {
+        const affectedCount = await Supply.update(supplyData, { where: { id } })
+        return affectedCount[0] > 0;
+    }
+}
+
+export default SupplyRepository
