3801cd321eb432646ada54d0524e4a546f296148
egota1n <71014753+egortanachev@users.noreply.github.com>
2023-06-14 23:38:13 +0300

    LW1 Done!

---
+++ labs/K33412/Tanachev Egor/LW1/task/src/routes/v1/users/User.ts
@@ -0,0 +1,33 @@
+import express from "express"
+import UserController from "../../../controllers/users/User"
+import passport from "../../../middlewares/passport"
+
+const router: express.Router = express.Router()
+
+const controller: UserController = new UserController()
+
+router.route('/')
+    .post(controller.post)
+
+router.route('/id/:id')
+    .get(controller.get)
+
+router.route('/email/:email')
+    .get(controller.getByEmail)
+
+router.route('/all')
+    .get(controller.getAll)
+
+router.route('/login')
+    .post(controller.auth)
+
+router.route('/profile')
+    .get(passport.authenticate('jwt', { session: false }), controller.me)
+
+router.route('/refresh')
+    .post(controller.refreshToken)
+
+    
+export default router
+
+
