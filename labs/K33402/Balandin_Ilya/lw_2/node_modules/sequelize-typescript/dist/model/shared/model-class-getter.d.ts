a8008dfa625950bbee825f856bf8ead54208717e
yaiBalan <imexikashka633@gmail.com>
2023-06-19 17:13:32 +0300

    delete node_modules

--- labs/K33402/Balandin_Ilya/lw_2/node_modules/sequelize-typescript/dist/model/shared/model-class-getter.d.ts
+++
@@ -1,2 +0,0 @@
-import { ModelType } from '../model/model';
-export declare type ModelClassGetter<TCreationAttributes extends {}, TModelAttributes extends {}> = (returns?: void) => ModelType<TCreationAttributes, TModelAttributes>;
--- labs/K33402/Balandin_Ilya/lw_3/auth/node_modules/sequelize-typescript/dist/associations/foreign-key/foreign-key.d.ts
+++
@@ -1,2 +0,0 @@
-import { ModelClassGetter } from '../../model/shared/model-class-getter';
-export declare function ForeignKey<TCreationAttributes extends {}, TModelAttributes extends {}>(relatedClassGetter: ModelClassGetter<TCreationAttributes, TModelAttributes>): Function;
