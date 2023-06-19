d64e42be8a27776e07734da66501c7bad84a2f96
David Dobryakov <davidobryakov@gmail.com>
2023-06-19 19:59:11 +0300

    ref: delete all node_modules from repo

--- labs/K33412/Traore_Mamoudou/Lab_4/LR3/login/node_modules/sequelize-typescript/dist/associations/foreign-key/foreign-key-meta.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModelClassGetter } from '../../model/shared/model-class-getter';
-export interface ForeignKeyMeta<TCreationAttributes extends {}, TModelAttributes extends {}> {
-    relatedClassGetter: ModelClassGetter<TCreationAttributes, TModelAttributes>;
-    foreignKey: string;
-}
