8120916b75abd92a24516baad8170e956c44582d
Оксана Панкратова <pankratova.oksana0220@gmail.com>
2023-06-26 02:05:04 +0300

    lw 2 done

---
+++ labs/K33401/Pankratova_Oksana/lw1/routes/user/user.ts
@@ -0,0 +1,33 @@
+import express from "express"
+import UserController from "../../controllers/user"
+import passport from "../../middlewares/passport"
+
+const router: express.Router = express.Router()
+
+const controller: UserController = new UserController()
+
+router.route('/signup')
+    .post(controller.post)
+
+router.route('/profile')
+    .get(passport.authenticate('jwt', { session: false }), controller.me)
+
+router.route('/profile/:id')
+    .get(controller.get)
+
+router.route('/login')
+    .post(controller.auth)
+
+router.route('/refresh')
+    .post(controller.refreshToken)
+
+router.route('/profiles')
+    .get(controller.getAll)
+
+router.route('/profiles/username/:username')
+    .get(controller.getByUsername)
+
+router.route('/profiles/email/:email')
+    .get(controller.getByEmail)
+
+export default router
---
+++ labs/K33401/Pankratova_Oksana/lw1/services/user.ts
@@ -0,0 +1,60 @@
+import User from '../models/user'
+import UserError from '../errors/user'
+import checkPassword from '../utils/checkPassword'
+import { use } from 'passport'
+
+class UserService {
+    async getById(id: number) : Promise<User> {
+        const user = await User.findByPk(id)
+
+        if (user) return user.toJSON()
+
+        throw new UserError('Not found!')
+    }
+
+    async create(userData: Partial<User>): Promise<User> {
+        try {
+            const user = await User.create(userData)
+
+            return user.toJSON()
+        } catch (e: any) {
+            const errors = e.errors.map((error: any) => error.message)
+
+            throw new UserError(errors)
+        }
+    }
+
+    async getAll() {
+        const users = await User.findAll()
+
+        if (users) return users
+
+        throw new UserError('Users are not found')
+    }
+
+    async getByUsername(username: string) {
+        const user = await User.findOne({where: {username: username}})
+
+        if (user) return user.toJSON()
+
+        throw new UserError('User with such username is not found')
+    }
+
+    async getByEmail(email: string) {
+        const user = await User.findOne({where: {email: email}})
+
+        if (user) return user.toJSON()
+
+        throw new UserError('User with such email is not found')
+    }
+
+    async checkPassword(email: string, password: string) : Promise<any> {
+        const user = await User.findOne({ where: { email } })
+
+        if (user) return { user: user.toJSON(), checkPassword: checkPassword(user, password) }
+
+        throw new UserError('Incorrect login/password!')
+    }
+}
+
+export default UserService
