18c2a8121a6b2893a3cff2c2c07c0994d5ae320f
ILIA CHUB <ilia.chub@dev.mobileup.ru>
2023-09-09 12:29:41 +0300

    Chub Ilia K33401 LW1. Part 8

--- labs/K33401/Chub_Ilia/LW1/src/middlewares/passport.ts
+++ labs/K33401/Chub_Ilia/LW1/src/middlewares/passport.ts
@@ -1,29 +1,38 @@
-import passport from 'passport'
-import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
-import UserService from '../services/userService'
-import dotenv from "dotenv"
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import UserService from '../services/userService';
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
+/**
+ * This module configures Passport.js for JWT (JSON Web Token) authentication.
+ */
+
+// Options for JWT authentication strategy.
 const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: 'secret',
+    secretOrKey: 'secret', // Replace with your actual secret key
     jsonWebTokenOptions: {
         maxAge: `${process.env.ACCESS_TOKEN_LIFETIME}ms`
     }
-}
+};
 
-const customJwtStrategy = new JwtStrategy(opts, async function(jwt_payload, next) {
-    const userService = new UserService()
-    const user = await userService.getById(jwt_payload.id)
+// Create a custom JWT strategy for Passport.js.
+const customJwtStrategy = new JwtStrategy(opts, async function (jwt_payload, next) {
+    const userService = new UserService();
+    const user = await userService.getById(jwt_payload.id);
+
     if (user) {
-        next(null, user)
+        next(null, user); // Successful authentication
     } else {
-        next(null, false)
+        next(null, false); // Authentication failed
     }
-})
+});
 
-passport.use(customJwtStrategy)
-export { opts as jwtOptions }
+// Use the custom JWT strategy with Passport.js.
+passport.use(customJwtStrategy);
 
-export default passport
+export { opts as jwtOptions };
+
+export default passport;
