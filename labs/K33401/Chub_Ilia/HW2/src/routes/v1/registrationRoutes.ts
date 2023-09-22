978c070fc59d5100829e7c5815a0a685356e750c
ILIA CHUB <ilia.chub@dev.mobileup.ru>
2023-09-23 00:00:25 +0300

    Chub Ilia K33401 LW2. Part 8

--- labs/K33401/Chub_Ilia/HW2/src/routes/v1/registrationRoutes.ts
+++ labs/K33401/Chub_Ilia/HW2/src/routes/v1/registrationRoutes.ts
@@ -1,15 +1,16 @@
 import express from "express";
 import RegistrationController from "../../controllers/registrationController";
+import passport from "../../middlewares/passport";
 
 const registrationRoutes: express.Router = express.Router();
-const controller: RegistrationController = new RegistrationController();
+const controller: RegistrationController = new RegistrationController();    
 
 registrationRoutes.route('/')
-    .post(controller.create)
+    .post(passport.authenticate('jwt', {session: false}), controller.create)
     .get(controller.getAll);
 
 registrationRoutes.route('/:id')
-    .get(controller.get)
-    .delete(controller.delete)
+    .get(passport.authenticate('jwt', {session: false}), controller.get)
+    .delete(passport.authenticate('jwt', {session: false}), controller.delete)
 
 export default registrationRoutes;
